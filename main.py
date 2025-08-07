2bfd0125f8b6c3e3caae1f6a1240e8b44bf2cda2
gbonez <gruehlmann2@gmail.com>
2025-08-07 02:25:04 -0400

    add db url error log

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import os
 # DATABASE setup
 # -------------------------------
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./subway_rides.db")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
 
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
